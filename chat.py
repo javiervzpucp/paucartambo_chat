fab7b6063b0b663b5bd88517aec6aea346447877
javiervzpucp <jveraz@pucp.edu.pe>
2024-11-17 01:00:53 -0500

    Update chat.py

--- chat.py
+++ chat.py
@@ -14,13 +14,14 @@ from neo4j import GraphDatabase
 import openai
 # Cargar las variables de entorno desde el archivo .env
 load_dotenv()
-openai_api_key = os.getenv("OPENAI_API_KEY")
+#openai_api_key = os.getenv("OPENAI_API_KEY")
 neo4j_url = os.getenv("NEO4J_URI")
 neo4j_user = os.getenv("NEO4J_USERNAME")
 neo4j_pass = os.getenv("NEO4J_PASSWORD")
 
 
 # Configurar el cliente de OpenAI
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 openai.api_key = openai_api_key
 
 # Inicializar cliente de OpenAI
